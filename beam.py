9da583c2081f6967b6aad8dd735fdf5d82375ae0
MEMAndersen <MEMAndersen@gmail.com>
2019-04-28 08:39:20 +0200

    general update

--- beam.py
+++ beam.py
@@ -19,8 +19,10 @@ class Beam:
 
         # Determine if 2D or 3D beam
         if node1.dim == 2:
+            self.dim = 2
             self.dof = 6
         elif node1.dim == 3:
+            self.dim = 3
             self.dof = 9
 
     def get_length(self):
@@ -28,6 +30,19 @@ class Beam:
 
     def d_matrix(self):
         return np.array([[self.e_module * self.area, 0], [0, self.e_module * self.moment_inertia]])
+
+    def n_matrix(self, xl):
+        le = self.length
+        xl_le = xl / le
+
+        n1 = 1 - xl_le
+        n2 = 1 - 3 * xl_le ** 2 + 2 * xl_le ** 3
+        n3 = le * (xl_le - 2 * xl_le ** 2 + xl_le ** 3)
+        n4 = xl_le
+        n5 = 3 * xl_le ** 2 - 2 * xl_le ** 3
+        n6 = le * (-xl_le ** 2 + xl_le ** 3)
+
+        return np.array([[n1, 0, 0, n4, 0, 0], [0, n2, n3, 0, n5, n6]])
 
     def b_matrix(self, xl):
         L = self.length
@@ -53,14 +68,80 @@ class Beam:
 
         return k
 
+    def t_matrix(self, typ='full'):
+        """
+            method: t_matrix(self, full=True)
 
-"""
-    def calc_strains(self, v, coords):
-        coords = np.zeros([100, 2])
-        xl = np.linspace(0, 1, coords)
-        for i in range(coords):
-            b = self.b_matrix(xl[i])
-            coords[i, :] = np.dot(b, v)
+            Transformation matrix.
 
-    def calc_stresses(self):
-"""
+            Parameters
+            ----------
+            typ='full':
+                Determines what kind of transformation matrix are provided.
+                'full' : Full transformation matrix for element
+                'single' : transformation for only one node
+
+            Returns
+            -------
+            xp : numpy.array of floats
+                Integration points
+            wp : numpy.array of floats
+                Weights
+            coords : int
+                Number of integration points and weights
+            """
+        n = 1 / self.length * (self.node2.coords - self.node1.coords)
+        nx = n[0]
+        ny = n[1]
+
+        if typ == 'full':
+            return np.array([[nx, ny, 0, 0, 0, 0],
+                             [-ny, nx, 0, 0, 0, 0],
+                             [0, 0, 1, 0, 0, 0],
+                             [0, 0, 0, nx, ny, 0],
+                             [0, 0, 0, -ny, nx, 0],
+                             [0, 0, 0, 0, 0, 1]])
+        elif typ == 'single':
+            return np.array([[nx, ny, 0],
+                             [-ny, nx, 0],
+                             [0, 0, 1]])
+        elif typ == 'node':
+            return np.array([[nx, ny],
+                             [-ny, nx]])
+
+    def plot(self, v, local=True, div=2):
+        # interpolate between nodes
+        x = np.linspace(self.node1.coords[0], self.node2.coords[0], div)
+        y = np.linspace(self.node1.coords[1], self.node2.coords[1], div)
+        xyz = np.transpose(np.array([x, y]))
+        xyz_v = np.zeros([div, self.dim])
+
+        # local length variable
+        xl = np.linspace(0, self.length, div)
+
+        # convert to local deformations
+        t_full = self.t_matrix()
+        if not local:
+            v = v.dot(t_full)
+
+        # compute points in local coordinates and transform back to global
+        t_node = self.t_matrix('node')
+        for i in range(div):
+            xyz_v[i, :] = self.n_matrix(xl[i]).dot(v).dot(t_node)
+
+        print(xyz)
+        plt.plot(xyz[:, 0], xyz[:, 1])
+        plt.plot(xyz[:, 0] + xyz_v[:, 0], xyz[:, 1] + xyz_v[:, 1])
+        plt.axis('equal')
+        plt.show()
+
+        """
+            def calc_strains(self, v, coords):
+                coords = np.zeros([100, 2])
+                xl = np.linspace(0, 1, coords)
+                for i in range(coords):
+                    b = self.b_matrix(xl[i])
+                    coords[i, :] = np.dot(b, v)
+        
+            def calc_stresses(self):
+        """
--- example.py
+++ example.py
@@ -11,13 +11,26 @@ from node import Node
 from beam import Beam
 
 n1 = Node(0, 0)
-n2 = Node(1, 1)
+n2 = Node(5, 0)
 
 beam1 = Beam(n1, n2, 1, 1, 1)
 
-v = np.transpose(np.array([0, 1, 0, 0, 0, 0]))
+v = np.transpose(np.array([0, 1, 1, 0, 0, 0]))
 
-print(beam1.k_matrix())
+k = beam1.k_matrix()
 
+f = k.dot(v)
 
+print(k,v, f)
+
+n = beam1.n_matrix(beam1.length)
+
+print(n)
+
+beam1.plot(v, local=True, div=100)
+
+t = beam1.t_matrix()
+print(t)
+
+print(v.dot(t))
 
